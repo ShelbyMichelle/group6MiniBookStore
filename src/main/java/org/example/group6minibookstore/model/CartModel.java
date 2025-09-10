e1bad870750268a993cb20418e09bcf5299290f7
barlondzowa <bit21-bdzowa@mubas.ac.mw>
2025-09-10 15:25:20 +0200

    yes

--- src/main/java/org/example/group6minibookstore/model/CartModel.java
+++ src/main/java/org/example/group6minibookstore/model/CartModel.java
@@ -2,25 +2,28 @@ package org.example.group6minibookstore.model;
 
 
 
+import jakarta.transaction.SystemException;
 import jakarta.transaction.Transaction;
 import org.example.group6minibookstore.entities.Book;
+import org.example.group6minibookstore.entities.Order;
 import org.example.group6minibookstore.entities.User;
+import org.example.group6minibookstore.utils.HibernateUtil;
 import org.hibernate.Session;
 
 import java.util.List;
 
-public class CartDAO {
+public class CartModel {
 
     private BookModel bookModel = new BookModel();
     private OrderModel orderDAO = new OrderModel();
 
     // Checkout: Convert session cart to Order
-    public void checkout(User user, List<Book> cart) {
+    public void checkout(User user, List<Book> cart) throws SystemException {
         if (cart == null || cart.isEmpty()) return;
 
         Transaction tx = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            tx = session.beginTransaction();
+            tx = (Transaction) session.beginTransaction();
 
             double totalAmount = cart.stream().mapToDouble(Book::getPrice).sum();
             Order order = new Order(user, cart, totalAmount);
@@ -28,15 +31,16 @@ public class CartDAO {
             // Save the order
             session.persist(order);
 
-            // Update stock for each book
             for (Book book : cart) {
-                Book dbBook = session.get(Book.class, book.getId());
-                dbBook.setStock(dbBook.getStock() - 1);
-                session.update(dbBook);
+                Book dbBook = session.find(Book.class, book.getId());
+                if (dbBook != null) {
+                    dbBook.setStock(dbBook.getStock() - 1);
+                }
             }
 
+
             tx.commit();
-            System.out.println("✅ Checkout completed, order saved!");
+            System.out.println("Checkout completed, order saved!");
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             e.printStackTrace();
